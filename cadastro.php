a39d4cf3950be2d37d9b409235081f70c266bf40
Miguel Luz <106433901+Migleu@users.noreply.github.com>
2023-06-15 20:37:40 -0300

    15/06

--- cadastro.php
+++ cadastro.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="./css/login.css">
+    <link rel="stylesheet" href="./css/cadastro.css">
     <title>Cadastro</title>
 </head>
 
@@ -16,12 +16,14 @@
             <form action="" method="post">
                 <div class="gp1">
                 <a href="./index.php"><i class="fa-sharp fa-solid fa-arrow-left"></i></a>
-                    <h1>Bem-vindo <span>Logue-se!</span></SPAn></h1>
+                    <h1>Bem-vindo <span>Cadastre-se!</span></SPAn></h1>
                 </div>
                 <div class="gp2">
                     <input name="email" type="email" placeholder="Email"><br>
                     <input name="senha" type="password" placeholder="Senha"><br>
+                    <input name="confirmsenha" type="password" placeholder="Confirme sua Senha"><br>
                     <input type="submit" nam="submit" value="Logar">
+                    <p><a href="./login.php">Já tenho uma conta!</a></p>
                 </div>
             </form>
         </div>
--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="shortcut icon" href="./img/icon-apple.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="./img/apple_icon.png" type="image/x-icon">
     <title>NutriMax</title>
 </head>
 <body>
@@ -18,7 +18,7 @@
         <div class="links">
             <ul class="nav-itens">
                <li><a href="#">Inicio</a></li>
-               <li id="nutri" onclick="nutri()"><a href="#">Nutricionista</a></li>
+               <li id="nutri" onclick="nutricionista()"><a href="#">Nutricionista</a></li>
                <li id="calendario" onclick="calendario()"><a href="#">Calendario</a></li>
                <li><a href="#">Dietas</a></li>
             </ul>
@@ -31,9 +31,25 @@
     </nav>
 
     <main class="main" id="main">
-        
+        <div class="inicio">
+            <h1>Use nosso site para <span>organizar sua vida Fitness!</span></h1>
+            <p>Saiba mais sobre:</p>
+            <div class="elementos">
+                <div class="elemento">
+                    <p>Minha dieta</p>
+                </div>
+
+                <div class="elemento">
+                    <p>Nutricionista IA</p>
+                </div>
+
+                <div class="elemento">
+                    <p>Meu calendario</p>
+                </div>
+            </div>
+        </div>
     </main>
 
-    <script src="./js/teste.js"></script>
+    <script src="./js/script.js"></script>
 </body>
 </html> 
