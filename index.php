e104ffdf3f7c2d39b23a4630ca0fb44338b26907
Miguel Luz <106433901+Migleu@users.noreply.github.com>
2023-08-22 21:09:48 -0300

    22/08

--- index.php
+++ index.php
@@ -68,10 +68,77 @@
             </div>
         </div>
         <div class="img">
-            <img src="./img/img.webp" alt="">
+            <img src="https://cdn-icons-png.flaticon.com/512/5223/5223176.png" alt="">
         </div>
         </div>
-            <hr id="hr">
+        <hr id="hr">
+        <div class="container_programa">
+            <div class="programa_titulo">
+                <h1>Metodologia NutriMax</h1>
+            </div>
+            <div class="programa_subtitulo">
+                <p>Deixe no passado as tentativas frustradas de ser mais saudável,
+                    as dietas mirabolantes e os treinos sem resultados.
+                    Confira como funcionam os programas do NutriMax.
+                </p>
+            </div>
+            <div class="programa">
+                <div class="programa_elementos">
+                    <img src="https://cdn-icons-png.flaticon.com/512/815/815128.png" alt="">
+                    <h1>Sem desafios dolorosos</h1>
+                    <p>Metodologias e programas são desenvolvidos por uma IA especialista focada em tirar o corpo da estagnação, melhor nutrição, reprogramar o corpo para queimar mais gordura e transformá-la em massa magra.</p>
+                </div>
+                <div class="programa_elementos">
+                    <img src="https://cdn-icons-png.flaticon.com/512/977/977451.png" alt="">
+                    <h1>Chegue no seu objetivo</h1>
+                    <p>Metodologias e programas são desenvolvidos por uma IA especialista focada em tirar o corpo da estagnação, melhor nutrição, reprogramar o corpo para queimar mais gordura e transformá-la em massa magra.</p>
+                </div>
+                <div class="programa_elementos">
+                    <img src="https://cdn-icons-png.flaticon.com/256/606/606534.png" alt="">
+                    <h1>melhore seu jeito de viver</h1>
+                    <p>Metodologias e programas são desenvolvidos por uma IA especialista focada em tirar o corpo da estagnação, melhor nutrição, reprogramar o corpo para queimar mais gordura e transformá-la em massa magra.</p>
+                </div>
+            </div>
+            <button id="programa_btn">Conheça nossos programas</button>
+        </div>
+        
+        <div class="container_feedback">
+            <h2>Observe alguns Feedbacks feitos para nosso site!</h2>
+            <div class="feedback">
+                <div class="feedback_elemento">
+                    <div class="feedback_user">
+                    <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" alt="">
+                        <h3>Roger Santos</h3>
+                    </div>
+                    <p>Muito bom!! Consegui resultados incriveis em poucos dias.</p>
+                    <img id="stars" src="https://www.tecnonutri.com.br/campanha/15em3/img/tecnonutri/fivestars.png" alt="">
+                </div>
+                <div class="feedback_elemento">
+                    <div class="feedback_user">
+                    <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" alt="">
+                        <h3>Roger Santos</h3>
+                    </div>
+                    <p>Muito bom!! Consegui resultados incriveis em poucos dias.</p>
+                    <img id="stars" src="https://www.tecnonutri.com.br/campanha/15em3/img/tecnonutri/fivestars.png" alt="">
+                </div>
+                <div class="feedback_elemento">
+                    <div class="feedback_user">
+                    <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" alt="">
+                        <h3>Roger Santos</h3>
+                    </div>
+                    <p>Muito bom!! Consegui resultados incriveis em poucos dias.</p>
+                    <img id="stars" src="https://www.tecnonutri.com.br/campanha/15em3/img/tecnonutri/fivestars.png" alt="">
+                </div>
+                <div class="feedback_elemento">
+                    <div class="feedback_user">
+                    <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" alt="">
+                        <h3>Roger Santos</h3>
+                    </div>
+                    <p>Muito bom!! Consegui resultados incriveis em poucos dias.</p>
+                    <img id="stars" src="https://www.tecnonutri.com.br/campanha/15em3/img/tecnonutri/fivestars.png" alt="">
+                </div>
+            </div>
+        </div>
     </main>
 
 </body>
